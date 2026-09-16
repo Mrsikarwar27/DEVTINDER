a13d4924a1114de4fc1d91ff19311fb1fd6100fe
Mrsikarwar27 <vijaysikarwar975267@gmail.com>
2026-09-16 23:22:43 +0530

    feat: add user model and signup route, configure MongoDB connection

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
 const express = require('express');
+const connectDB = require("./src/config/database");
+const User = require("./src/models/user");
+require("dotenv").config()
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
-// Sample route
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
+//add to the user in db
+
+app.post("/signup", async (req, res) => {
+
+  const user = new User({
+    firstName: "vijay",
+    lastName: "sikarwar",
+    emailID: "vijay@gmail.com",
+    password: "vijay12",
+
+
+  })
+
+  await user.save()
+
+  res.send("user is successfully resgistered")
+
+})
+
+
+//first connect to db then start the server
+connectDB().then(() => {
+  console.log('Connected to MongoDB');
+
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server is running port:${port}`);
+  });
+
+
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-}); 
+
+
+
